8cc0ae376f8ba016a69e83eca2b3ff85ec30f0e6
nistha sarawagi <gunnistha@gmail.com>
2026-05-10 23:45:54 +0530

    Set TRANSFORMERS_CACHE environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+process.env.TRANSFORMERS_CACHE = "/tmp/transformers_cache";  
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
@@ -25,7 +26,7 @@ const app = express();
 //   methods: ["GET", "POST"],
 //   allowedHeaders: ["Content-Type"]
 // }))
-process.env.TRANSFORMERS_CACHE = "/tmp/transformers_cache";  
+
 app.use(cors());
 
 app.use(express.json());
